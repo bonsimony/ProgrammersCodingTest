1462e05fd900bc001befb783cdbb39b1235e1a5e
bonsimony <hbon7270@gmail.com>
2024-09-27 11:56:31 +0900

    2024-09-27 KHB
    프로그래머스 2단계 올바른괄호

--- src/level2/CorrectParentheses.java
+++ src/level2/CorrectParentheses.java
@@ -1,4 +1,81 @@
 package level2;
 
 public class CorrectParentheses {
+
+    public static void main(String[] args) {
+
+        //String s = "()()";
+        //String s = "(())()";
+        //String s = ")()(";
+        String s = "(()(";
+
+        solution(s);
+
+        System.out.println(solution(s));
+    }
+
+    public static boolean solution(String s) {
+        //boolean answer = true;
+
+        int leftParenthesesCount;
+        int rightParenthesesCount;
+        int i;
+        char leftParentheses;
+        char rightParentheses;
+
+        leftParenthesesCount = 0;
+        rightParenthesesCount = 0;
+
+        leftParentheses = '(';
+        rightParentheses = ')';
+
+        // 갯수
+        // ()()     // ( -> 2 // ) -> 2
+        // (())()   // ( -> 3 // ) -> 3
+        // )()(     // ( -> 2 // ) -> 2
+        // (()(     // ( -> 3 // ) -> 1
+
+        for(i =0; i < s.length(); i++){
+
+            if(s.charAt(i) == leftParentheses){
+
+                leftParenthesesCount ++;
+            }
+
+            if(s.charAt(i) == rightParentheses){
+
+                rightParenthesesCount ++;
+            }
+
+            if(leftParenthesesCount < rightParenthesesCount){
+
+                //answer = false;
+                //return answer;
+
+                return false;
+            }
+
+        }
+
+
+//        if(leftParenthesesCount == rightParenthesesCount){
+//
+//                //answer = true;
+//                //return answer;
+//
+//                return true;
+//            }else {
+//
+//                //answer = false;
+//                //return answer;
+//
+//                return false;
+//        }
+
+        return leftParenthesesCount == rightParenthesesCount;
+
+
+
+    }
+
 }
