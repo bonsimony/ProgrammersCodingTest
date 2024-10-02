ef75c207db0343df1493a4ccaf32f8848341db27
bonsimony <hbon7270@gmail.com>
2024-10-02 15:49:54 +0900

    2024-10-02 KHB
    프로그래머스 0단계 문자열의 앞의 n글자

--- src/level0/nLetterBeforeString.java
+++ src/level0/nLetterBeforeString.java
@@ -5,7 +5,7 @@ public class nLetterBeforeString {
     // 코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열의 앞의 n글자
     //
     // 문제 설명
-    // 문자열 my_string과 정수 n이 매개변수로 주어질 때,
+    // 문자열 my_string과 정수 n이 매개변수로 주어질 때 ,
     // my_string의 앞의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
 
     public static void main(String[] args) {
