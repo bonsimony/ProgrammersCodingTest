fac2c004ca86968f119dd3549f6fe2034ff3994c
bonsimony <hbon7270@gmail.com>
2024-10-04 13:18:22 +0900

    2024-10-04 KHB
    프로그래머스 0단계 접미사 확인하기 응용해보기
    : endsWith 함수 사용

---
+++ src/level0/suffix/EndsWith.java
@@ -0,0 +1,15 @@
+package level0.suffix;
+
+public class EndsWith {
+
+    public static void main(String[] args) {
+
+        String str = "endswith";
+
+        boolean result = str.endsWith("with");
+
+        System.out.println(result);
+
+    }
+
+}
